7cc674c2bb13fbfa7f6316bb95bf591e03f96609
khomsandev <bas.rmu60@gmail.com>
2025-11-06 13:43:21 +0700

    edit docker compose

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "http://localhost:5173",
+        // target: "http://localhost:5173",
+        target: "http://backend:5173",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
